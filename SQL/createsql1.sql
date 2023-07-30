bdc8c97c4fae1e5acb6af6b5362e4a042df69327
dhavalgupta10 <56255084+dhavalgupta10@users.noreply.github.com>
2023-07-30 13:57:16 +0530

    Add files via upload

---
+++ SQL/createsql1.sql
@@ -0,0 +1,26 @@
+--liquibase formatted sql
+--changeset Dhaval:9998
+--preconditions onFail:CONTINUE
+--comment: Initial creation of table Dhaval
+--validCheckSum: ANY
+Create Table Dhaval (
+   ID INTEGER,
+   NAME VARCHAR(10),
+   PRIMARY KEY (ID)
+);
+ 
+--rollback drop table Dhaval
+
+
+--liquibase formatted sql
+--changeset Dhaval:999
+--preconditions onFail:CONTINUE
+--comment: Initial creation of table Dhaval
+--validCheckSum: ANY
+--Create Table Payments (
+  -- ID INTEGER,
+   --NAME VARCHAR(10),
+  -- PRIMARY KEY (ID)
+--);
+ 
+--rollback drop table Payments;
